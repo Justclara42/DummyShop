981c9e2db083ffc0c2aa33d0b8d9a0752631a91d
Justclara42 <clara.holderbaum@icloud.com>
2025-06-19 11:52:19 +0200

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", (event) => {
+    fetchProducts();
+});
+async function fetchProducts() {
+    try {
+        const url = 'https://dummyjson.com/products/category/laptops';
+        const response = await axios.get(url);
+        const products = await response.data.products;
+        getProducts(products);
+    }catch(error){
+        console.log(error);
+    }
+
+}
+
+function getProducts(products){
+    const container = document.querySelector('#productsList');
+    container.innerHTML = '';
+    products.forEach(product => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.classList.add('m-2');
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+          <img src=${product.thumbnail} class="card-img-top" alt="${product.name}">
+          <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+            <span class="badge text-bg-primary">${product.category}</span>
+            <p class="card-text">${product.description}</p>
+            <p class="card-text">Prix: ${product.price} $ Discount: ${product.discountPercentage} %</p>
+            ${product.tags.map(tag =>
+            ` <span class="badge text-bg-secondary">${tag}</span> `
+        ).join('')}
+            <a href="http://localhost:63342/DummyAPI/product.html?id=${product.id}"  class="btn btn-primary">Voir le Produit</a>
+          </div>
+        </div>
+        `;
+        container.appendChild(div);
+
+    })
+}
+
---
+++ assets/js/product.js
@@ -0,0 +1,42 @@
+const url = new URL(document.location);
+const searchParams = url.searchParams;
+const id = searchParams.get('id');
+
+window.addEventListener("load", (event) => {
+    productItem(id);
+});
+
+async function productItem(id){
+    const url = `https://dummyjson.com/products/${id}`;
+    const response = await axios.get(url);
+    const item = await response.data;
+    console.log(item);
+    const container = document.querySelector('#productsItem');
+    container.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.classList.add('m-2');
+    div.innerHTML = `
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 rounded text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">${item.title}</h2>
+                <img class="img-fluid" src="${item.thumbnail}" alt="Responsive image"/>
+                <h2 class="display-5">${item.brand}</h2>
+                <p class="lead">${item.warrantyInformation}</p>
+                <p class="lead">Note de ${item.rating}/5</p>
+                <p>${item.description}</p>
+                <p class="lead">Notre prix: ${item.price} €</p>
+                <span class="badge text-bg-success">Vous economisez ${item.discountPercentage} %</span>
+
+            </div>
+            <div class="bg-dark box-shadow rounded mx-auto text-white">
+                ${item.images.map(image =>
+                    `<img class="img-fluid" src="${image}" alt="Responsive image">`
+                ).join(' ')}
+            </div>
+        </div>    
+    `;
+    container.appendChild(div);
+
+}
